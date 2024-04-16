25fd86ea6b645bd4cf25a033c0b55c1564c0dc25
nkcheung <nkcheung@ualberta.ca>
2024-04-16 10:35:16 -0600

    Create shinyloader.R

---
+++ shinyloader.R
@@ -0,0 +1,25 @@
+packages <- c(
+  "shiny"
+)
+
+# Function to install and load packages
+install_load_packages <- function(packages) {
+  # Check which packages are not installed
+  not_installed <- setdiff(packages, rownames(installed.packages()))
+  
+  # Install the missing packages
+  if (length(not_installed) > 0) {
+    install.packages(not_installed)
+  }
+  
+  # Load all the packages
+  invisible(sapply(packages, library, character.only = TRUE))
+}
+
+# Call the function to install and load packages
+install_load_packages(packages)
+# Define the path to your Shiny app directory
+app_dir <- "./app.R"
+
+# Run the Shiny app
+runApp(appDir = app_dir)
